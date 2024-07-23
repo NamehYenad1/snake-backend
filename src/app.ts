7513502f2410511b682418ece0043d648650b652
NamehYenad1 <Hemandaney@yahoo.com>
2024-07-23 13:31:18 +0800

    updated packages

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import gameRoutes from './routes/gameRoutes';
+var cors = require('cors')
 
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use('/api/game', gameRoutes);
 
